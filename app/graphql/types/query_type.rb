627652e56b5528d335031712240f0619b723d431
hasanmahira <hasanmahira@gmail.com>
2022-02-14 01:19:20 +0300

    somekindly working login

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,14 +1,12 @@
 module Types
   class QueryType < Types::BaseObject
+    # Add `node(id: ID!) and `nodes(ids: [ID!]!)`
+    # include GraphQL::Types::Relay::HasNodeField
+    # include GraphQL::Types::Relay::HasNodesField
+    # include GraphQL::Types::Relay::NodeField
+
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
-
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
-    end
 
        # User Login
        field :login, String, null: true, description: "Login a user" do
@@ -17,8 +15,9 @@ module Types
       end
   
       def login(email:, password:)
-        if user = User.where( email: email).first&.authenticate(password)
-         user.sessions.create.key
+        if user = User.where( email: email).first 
+          "Hello " + email
+        #  user.sessions.create.key
         end
       end
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,30 @@
 class User
   include Mongoid::Document
+  include Mongoid::Timestamps
+  include BCrypt
+
+  field :id, type: String
+  field :user_name, type: String
   field :name, type: String
+  field :surname, type: String
   field :email, type: String
+  field :password_digest, type: String
+  field :bdate, type: String
+  field :created_at, type: String
+  field :updated_at, type: String
+
+  # has_secure_password
+
+  before_save :encrypt_password
+
+  def authenticate(password)
+    Password.new(password_hash) == password
+  end
+
+  protected
+
+  def encrypt_password
+    self.password_hash = Password.create(@password)
+  end
+
 end
