46c43196024122b4baf9ef60319b5179b7f7624e
hasanmahira <hasanmahira@gmail.com>
2022-02-16 23:19:40 +0300

    nft file queries

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -8,8 +8,8 @@ module Types
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
 
-       # User Login
-       field :login, String, null: true, description: "Login a user" do
+      # User Login
+      field :login, String, null: true, description: "Login a user" do
         argument :email, String, required: true
         argument :password, String, required: true
       end
@@ -20,5 +20,25 @@ module Types
         #  user.sessions.create.key
         end
       end
+
+      # getNftFiles
+      field :get_nft_files, [NftFileOutput], null: true, description: "Get Nft Files" do
+      end
+  
+      def get_nft_files()
+        file = NftFile.all
+      end
+
+      # getNftFilesByCategory
+      field :get_nft_files_by_category, [NftFileOutput], null: true, description: "Get Nft Files" do
+        argument :category, Int, required: false
+      end
+  
+      def get_nft_files_by_category(category:)
+        if category > 0 
+          file = NftFile.all.where(category: category)
+        end
+      end
+
   end
 end
--- app/models/nft_file.rb
+++ app/models/nft_file.rb
@@ -1,5 +1,7 @@
 class NftFile
   include Mongoid::Document
+  include Mongoid::Timestamps
+
   field :creator, type: String
   field :name, type: String
   field :price, type: Float
@@ -10,4 +12,11 @@ class NftFile
   field :cid, type: String
   field :created_at, type: String
   field :updated_at, type: String
+
+  before_save :encrypt_password
+
+  def encrypt_password
+    self.created_at = Date.today
+    self.updated_at = Time.now
+  end
 end
